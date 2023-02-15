188f9f3d76b80a627be7161e1ac5d03237ae6918
Mayank Patel <mayank@tb060.wpa.dal.ca>
2023-02-15 18:27:19 -0400

    Added delivery and polling entity

--- src/main/java/com/gomeals/controller/DeliveryController.java
+++ src/main/java/com/gomeals/controller/DeliveryController.java
@@ -1,9 +1,12 @@
 package com.gomeals.controller;
 
 import com.gomeals.model.Delivery;
+import com.gomeals.repository.DeliveryRepository;
 import com.gomeals.service.DeliveryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/delivery")
@@ -17,9 +20,24 @@ public class DeliveryController {
         return deliveryService.getDeliveryById(id);
     }
 
+    @GetMapping("/get/customer/{id}")
+    public List<Delivery> getByCustomerId(@PathVariable int id) {
+        return deliveryService.getByCustId(id);
+    }
+
     @PostMapping("/create")
     public Delivery createDelivery(@RequestBody Delivery delivery) {
         return deliveryService.createDelivery(delivery);
+    }
+
+    @PutMapping("/update")
+    public Delivery updateDelivery(@RequestBody Delivery delivery) {
+        return deliveryService.updateDelivery(delivery);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteDeliveryById(@PathVariable int id) {
+        return deliveryService.deleteDeliveryById(id);
     }
 
 }
--- src/main/java/com/gomeals/service/implementation/DeliveryServiceImplementation.java
+++ src/main/java/com/gomeals/service/implementation/DeliveryServiceImplementation.java
@@ -5,6 +5,9 @@ import com.gomeals.repository.DeliveryRepository;
 import com.gomeals.service.DeliveryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
 public class DeliveryServiceImplementation implements DeliveryService {
@@ -22,5 +25,28 @@ public class DeliveryServiceImplementation implements DeliveryService {
     public Delivery getDeliveryById(int id) {
         Delivery delivery = deliveryRepository.findById(id).orElse(null);
         return delivery;
+    }
+
+    @Override
+    public Delivery updateDelivery(@RequestBody Delivery delivery) {
+        Delivery currentDelivery = deliveryRepository.findById(delivery.getDeliveryId()).orElse(null);
+        currentDelivery.setDeliveryDate(delivery.getDeliveryDate());
+        currentDelivery.setDeliveryMeal(delivery.getDeliveryMeal());
+        currentDelivery.setOrderStatus(delivery.getOrderStatus());
+        currentDelivery.setSupId(delivery.getSupId());
+        currentDelivery.setCustId(delivery.getCustId());
+        deliveryRepository.save(currentDelivery);
+        return currentDelivery;
+    }
+
+    @Override
+    public String deleteDeliveryById(int id) {
+        deliveryRepository.deleteById(id);
+        return "Delivery deleted!";
+    }
+
+    @Override
+    public List<Delivery> getByCustId(int id) {
+        return deliveryRepository.findByCustId(id);
     }
 }
